80caeaf4c9768a0712c4fb1c97783d9430889fbc
Daniel Sanchez <daniel.sanchez@qacg.com>
2024-11-20 18:37:36 -0600

    Initial Commit

---
+++ Movieflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movieflix
+//
+//  Created by Iván Sánchez Torres on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
